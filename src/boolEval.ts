7a226c8c58c4b394e0d4308430f1ad3d39a269a1
carmon <elcarmon@gmail.com>
2020-11-09 20:49:35 -0300

    Update to live stream

--- src/boolEval.ts
+++ src/boolEval.ts
@@ -1,6 +1,11 @@
 import { BooleanEval, Option } from './types';
 
 
+const base: BooleanEval = (a, b, c) => {
+    'M': (d, e, _) => d + (d * e / 10),
+    'P': (d, e, f) => d + (d * (e + f) / 25.5),
+    'T': (d, _, f) => d - (d * f / 30),
+};
 
 export default (o: Option): BooleanEval => (a, b, c) => {
 
