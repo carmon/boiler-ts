91bedd9e5e61af640efde0c7278279e90a165720
Carmon <elcarmon@gmail.com>
2021-03-27 11:09:05 -0300

    Add simple react boilerplate

--- index.ts
+++
@@ -1,9 +0,0 @@
-import app from './src/index';
-
-console.log('Runnning App');
-console.log(app('base')(true, true, false, 10.5, 5, 2));
-console.log(app('custom1')(true, true, true, 10.5, 25, 2));
-console.log(app('custom2')(true, false, true, 0.5, 15, 120));
-
-// This will throw an error
-// console.log(app('base')(false, false, false, 0.5, 15, 120));
